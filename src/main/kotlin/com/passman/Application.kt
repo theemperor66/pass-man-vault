0fc6ba3edf2192379769775d8c89a483448ba2d1
zaid.marzguioui <zaid.marzguioui@internetx.com>
2023-04-01 00:55:33 +0200

    fixes

--- src/main/kotlin/com/passman/Application.kt
+++ src/main/kotlin/com/passman/Application.kt
@@ -1,20 +1,11 @@
 package com.passman
 
 import io.ktor.server.application.*
-import com.passman.plugins.*
 
 fun main(args: Array<String>): Unit =
     io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
 fun Application.module() {
-    configureSecurity()
-    configureHTTP()
-    configureMonitoring()
-    configureSockets()
-    configureAdministration()
-    configureSerialization()
-    configureDatabases()
-    configureTemplating()
-    configureRouting()
+
 }
